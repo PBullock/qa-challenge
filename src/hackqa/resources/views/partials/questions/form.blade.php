ef9449634b9691c375c3fdcf95f5fad644b9d3e2
PBullock <p.bullock@hanseatics.com>
2020-03-09 13:36:02 +0100

    Question error styles

--- src/hackqa/resources/views/partials/questions/form.blade.php
+++ src/hackqa/resources/views/partials/questions/form.blade.php
@@ -3,8 +3,16 @@
     <input name="question" id="question" placeholder="{{ $placeholder }}" value="{{ old('question', '') }}">
     @csrf
     @if($errors->any())
-        @foreach ($errors->all() as $error)
-            <div class="alert alert-danger"> {{ $error }}</div>
-        @endforeach
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert"> {{ $error }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     @endif
 </form>
