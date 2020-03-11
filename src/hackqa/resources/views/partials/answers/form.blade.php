bedefddd8bd234608725786e01e1a5249687ad15
PBullock <p.bullock@hanseatics.com>
2020-03-12 00:30:12 +0100

    Correct (invalid) answer original value display

--- src/hackqa/resources/views/partials/answers/form.blade.php
+++ src/hackqa/resources/views/partials/answers/form.blade.php
@@ -1,7 +1,7 @@
 <form action="/question/{{$question->question_id}}/answer" method="post">
     <div class="form-group">
         <label for="answer" class="font-weight-bold">The Answer is: </label>
-        <textarea class="form-control" name="answer" id="answer" placeholder="my answer is..." value="{{ old('answer', '')  }}"></textarea>
+        <textarea class="form-control" name="answer" id="answer" placeholder="my answer is..." >{{ old('answer', '')  }}</textarea>
         <button type="submit" class="btn btn-outline-dark mt-3">Post Answer</button>
     </div>
     @csrf
